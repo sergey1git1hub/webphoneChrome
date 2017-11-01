d7d3de05ccebd483a1892b121e33c31d7baab2de
SChubuk <nameserhiy@gmail.com>
2017-11-01 15:55:12 +0200

    update

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -342,7 +342,6 @@ public class Methods {
             System.out.println("Browser is not chrome or running on Jenkns.");
             if(hostName.equalsIgnoreCase("kv1-it-pc-jtest")){
                 WebElement lineElement = driver.findElement(By.cssSelector("[id = 'btn_line_" + line + "_span']"));
-                lineElement.clear();
                 lineElement.sendKeys(Keys.ENTER);
             }else
             try {
@@ -376,6 +375,8 @@ public class Methods {
             }
             System.out.println("Sikuli clkicked phone number filed.");
             WebElement phoneNumberField = driver.findElement(By.cssSelector("#PhoneNumber"));
+            phoneNumberField.click();
+            phoneNumberField.clear();
             phoneNumberField.sendKeys(number);
             System.out.println("Selenium send phone number key.");
             WebElement button_Call = driver.findElement(By.cssSelector("#btn_call"));
