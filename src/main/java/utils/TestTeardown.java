9d0f4c616ab2e15c0512e52cc81c6a8bd251b79d
SChubuk <nameserhiy@gmail.com>
2018-04-04 18:03:57 +0300

    IE window not closed.

--- src/main/java/utils/TestTeardown.java
+++ src/main/java/utils/TestTeardown.java
@@ -25,7 +25,7 @@ public class TestTeardown {
     public static void teardown(WebDriver driver, String testName) throws IOException, InterruptedException {
         saveLogs(driver, "b" + testName);
         logOut(driver);
-        System.out.println("closeBrowser: " + Boolean.getBoolean("closeBrowser"));
+        //System.out.println("closeBrowser: " + Boolean.getBoolean("closeBrowser"));
         if (Boolean.getBoolean("closeBrowser")) {
             Thread.sleep(2000);
             driver.quit();
@@ -54,6 +54,7 @@ public class TestTeardown {
             Thread.sleep(2000);
             driver.quit();
             boolean isIE = Flags.isIE(driver);
+            System.out.println("isIE: " + isIE);
             String hostName = InetAddress.getLocalHost().getHostName();
 
             if (isIE) {
