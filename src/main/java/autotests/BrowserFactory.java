5b8ad50b925bf56ebd31aabc8f1d9e83b50f2b51
SChubuk <nameserhiy@gmail.com>
2018-04-20 17:22:01 +0300

    Driver factory

--- src/main/java/autotests/BrowserFactory.java
+++ src/main/java/autotests/BrowserFactory.java
@@ -1,7 +1,78 @@
 package autotests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.logging.Level;
+
 /**
  * Created by SChubuk on 19.04.2018.
  */
 public class BrowserFactory {
+
+    private WebDriver driver;
+    private String browserName = System.getProperty("browserName");
+
+    public WebDriver getDriver(boolean remote) throws Exception{
+
+        if(browserName.equalsIgnoreCase("ie")){
+            System.setProperty("webdriver.ie.driver", "C:/iedriver32/IEDriverServer.exe");
+            DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
+            ieCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
+                    true);
+
+            /**********PLAY WITH CAPABILITIES*********************/
+            //ieCapabilities.setCapability("initialBrowserUrl", webphoneUrl);
+            ieCapabilities.setCapability("nativeEvents", false);
+            //ieCapabilities.setCapability("unexpectedAlertBehaviour", "accept");
+            ieCapabilities.setCapability("ignoreProtectedModeSettings", true);
+            ieCapabilities.setCapability("disable-popup-blocking", true);
+            ieCapabilities.setCapability("enablePersistentHover", true);
+            ieCapabilities.setCapability("ignoreZoomSetting", true);
+            /***************************************************/
+
+            driver = new InternetExplorerDriver(ieCapabilities);
+
+        }
+
+        if(browserName.equalsIgnoreCase("chrome")){
+            System.setProperty("webdriver.chrome.driver", "C:/seleniumdrivers/chromedriver.exe");
+
+            DesiredCapabilities caps = DesiredCapabilities.chrome();
+            LoggingPreferences logPrefs = new LoggingPreferences();
+            logPrefs.enable(LogType.BROWSER, Level.ALL);
+
+            caps.setCapability(CapabilityType.LOGGING_PREFS, logPrefs); //deprecated
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--start-maximized");
+            chromeOptions.merge(caps);
+
+            if (Boolean.getBoolean("autoOpenDevtoolsForTabs")) {
+                chromeOptions.addArguments("--auto-open-devtools-for-tabs");
+            }
+            chromeOptions.addArguments("--preserve-log");
+            if (remote) {
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+            } else {
+                driver = new ChromeDriver(chromeOptions);
+            }
+        }
+
+        if(browserName.equalsIgnoreCase("opera")){
+
+        }
+
+        return new ChromeDriver();
+    }
+
+
+
 }
